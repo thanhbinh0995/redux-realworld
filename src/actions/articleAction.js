ac63f5325efd809331340093a52b1105287a2633
Binh Pham <thanhbinh0995@gmail.com>
2017-11-18 10:56:11 +0700

    change error to login page

--- src/actions/articleAction.js
+++ src/actions/articleAction.js
@@ -2,14 +2,21 @@ import * as types from "./actionTypes";
 import axios from "axios";
 
 const ROOT_URL = 'https://conduit.productionready.io/api';
-const url = `${ROOT_URL}/articles?limit=10`;
-const request = axios.get(url);
+
 
 export function loadArticlesSuccess(articles) {
   return {type: types.LOAD_ARTICLES_SUCCESS, articles};
 }
 
+export function loadArticlesByUser(user) {
+  const url = `${ROOT_URL}/articles?author=thanhbinh`;
+  const request = axios.get(url);
+
+}
+
 export function loadArticles() {
+  const url = `${ROOT_URL}/articles?limit=10`;
+  const request = axios.get(url);
   return function (dispatch) {
     return request.then(request => {
       dispatch(loadArticlesSuccess(request.data.articles));
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import {Route, Router} from "react-router-dom";
-import {connect} from "react-redux";
+import { Route, Router } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {history} from "../helpers";
-import {alertActions} from "../actions";
-import {PrivateRoute} from "./PrivateRoute";
-import {HomePage} from "./home";
-import {LoginPage} from "./login";
-import {RegisterPage} from "./register";
-import {AboutPage} from "./about/AboutPage";
-import {ContactPage} from "./contact/ContactPage";
-import {ProfilePage} from "./profile/ProfilePage";
-import {ProfileEditPage} from "./profile/ProfileEditPage";
+import { history } from "../helpers";
+import { alertActions } from "../actions";
+import { PrivateRoute } from "./PrivateRoute";
+import { HomePage } from "./home";
+import { LoginPage } from "./login";
+import { RegisterPage } from "./register";
+import { AboutPage } from "./about/AboutPage";
+import { ContactPage } from "./contact/ContactPage";
+import { ProfilePage } from "./profile/ProfilePage";
+import { ProfileEditPage } from "./profile/ProfileEditPage";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    const {dispatch} = this.props;
+    const { dispatch } = this.props;
     history.listen((location, action) => {
       // clear alert on location change
       dispatch(alertActions.clear());
@@ -25,34 +25,23 @@ class App extends React.Component {
   }
 
   render() {
-    const {alert} = this.props;
     return (
-        <div>
-          <Router history={history}>
-            <div>
-              <PrivateRoute exact path="/" component={HomePage}/>
-              <Route path="/login" component={LoginPage}/>
-              <Route path="/register" component={RegisterPage}/>
-              <Route path="/about" component={AboutPage}/>
-              <Route path="/contact" component={ContactPage}/>
-              <Route exact path="/profile" component={ProfilePage}/>
-              <Route path="/profile/edit" component={ProfileEditPage}/>
-            </div>
-          </Router>
-          {alert.message &&
-          <div className={`alert ${alert.type}`}>{alert.message}</div>
-          }
-        </div>
+      <div>
+        <Router history={history}>
+          <div>
+            <PrivateRoute exact path="/" component={HomePage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/contact" component={ContactPage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route path="/profile/edit" component={ProfileEditPage} />
+          </div>
+        </Router>
+      </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  const {alert} = state;
-  return {
-    alert
-  };
-}
-
-const connectedApp = connect(mapStateToProps)(App);
-export {connectedApp as App};
+const connectedApp = connect()(App);
+export { connectedApp as App };
