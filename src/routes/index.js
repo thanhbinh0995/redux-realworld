0ac9627ba492e76543530ef9f18e5aba185b574d
Binh Pham <thanhbinh0995@gmail.com>
2017-11-22 16:10:24 +0700

    push

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import {PrivateRoute} from "../components/PrivateRoute";
-// import { HomePage } from "../components/home";
 import HomePage from '../components/home/HomePage';
 import LoginPage from "../components/login/LoginPage";
 import RegisterPage from "../components/register/RegisterPage";
@@ -11,13 +10,12 @@ import ProfilePage from "../components/profile/ProfilePage";
 
 const TopLevelRoutes = () => (
   <Switch>
-    <PrivateRoute exact path="/" component={HomePage} />    
+    <PrivateRoute exact path="/" component={HomePage} />   
+    <PrivateRoute path="/profile" component={ProfilePage} />     
     <Route exact path="/login" component={LoginPage} />
     <Route exact path="/register" component={RegisterPage} />
-    <Route path="/register" component={RegisterPage} />
     <Route path="/about" component={AboutPage} />
     <Route path="/contact" component={ContactPage} />
-    <Route path="/profile" component={ProfilePage} />    
   </Switch>
 );
 
